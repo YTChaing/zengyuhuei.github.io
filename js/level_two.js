c0fd993320d9674c14d8cfd03cc9c7df1c9b0737
cctingcc1009 <ccctingccc10009@gmail.com>
2018-06-16 14:36:09 +0800

    level2

--- js/level_two.js
+++ js/level_two.js
@@ -1,67 +1,118 @@
 
 $(document).ready(function() {
-		$(".answer").mouseover(function() {
-			$("#mylight").attr("src", "./img/hover_light.jpg");
-			var sound = document.getElementById("audio");
-			sound.play();
-		
-			sound.currentTime=0;
-		})
-		
-		$(".btn").mouseover(function() {
-		
-			var sound = document.getElementById("buttom");
-			 sound.play();
-		
-		
-		})
+	$(".answer").mouseover(function() {
+		$("#mylight").attr("src", "./img/hover_light.jpg");
+		var sound = document.getElementById("audio");
+		sound.play();
+
+		sound.currentTime=0;
+	})
 	
-		$(".answer").mouseout(function() {
-			$("#mylight").attr("src", "./img/light.png");
-		})
-		});
-		function isWin() {
-			$("#win").dialog("open");
-			//alert("Win");
-		}
-		$(function() {
-			$("#win").dialog({
-				closeOnEscape: false,
-				open: function(event, ui) {
-					$(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
-				},
-				draggable: false,
-				resizable: false,
-				autoOpen: false,
-				show: {
-					effect: "blind",
-					duration: 1000
-				},
-				hide: {
-					effect: "explode",
-					duration: 1000
-				},
-				modal: true,
-				width: 400
-			});
+	$(".btn").mouseover(function() {
 
-			$("#dialog").dialog({
-				draggable: false,
-				resizable: false,
-				autoOpen: false,
-				show: {
-					effect: "blind",
-					duration: 1000
-				},
-				hide: {
-					effect: "explode",
-					duration: 1000
-				},
-				modal: true,
-				width: 400
-			});
+		var sound = document.getElementById("buttom");
+		 sound.play();
+	})
+	$("#haha2").click(function() {
+		var sound2 = document.getElementById("laught");
+		sound2.play();
 
-			$("#prompt").on("click", function() {
-				$("#dialog").dialog("open");
-			});
-		});
+		sound2.currentTime=0;
+	})
+	$(".answer").mouseout(function() {
+		$("#mylight").attr("src", "./img/light.png");
+	})
+});
+function isWin() {
+	$("#win").dialog("open");
+	//alert("Win");
+}
+$(function() {
+	$("#win").dialog({
+		closeOnEscape: false,
+		open: function(event, ui) {
+			$(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
+		},
+		draggable: false,
+		resizable: false,
+		autoOpen: false,
+		show: {
+			effect: "blind",
+			duration: 1000
+		},
+		hide: {
+			effect: "explode",
+			duration: 1000
+		},
+		modal: true,
+		width: 400
+	});
+	$("#haha").dialog({
+		draggable: false,
+		resizable: false,
+		autoOpen: false,
+		show: {
+			effect: "blind",
+			duration: 1000,
+		},
+		hide: {
+			effect: "fade",
+			duration: 1000
+		},
+		modal: true,
+		width: 400
+		
+		
+	});
+	$("#dialog").dialog({
+		draggable: false,
+		resizable: false,
+		autoOpen: false,
+		show: {
+			effect: "blind",
+			duration: 1000
+		},
+		hide: {
+			effect: "explode",
+			duration: 1000
+		},
+		modal: true,
+		width: 400
+	});
+
+	$("#prompt").on("click", function() {
+		$("#dialog").dialog("open");
+	});
+	
+});
+
+function dropimage(event)
+{
+	console.log(event.srcElement.style.top);
+	var x = event.clientX;
+	var y = event.clientY;
+
+	event.srcElement.style.top = (parseInt(y)-300)+"px";
+	event.srcElement.style.left = (parseInt(x)-100)+"px";
+
+	
+}
+
+function dropword(event)
+{
+	console.log(event);
+	var x = event.clientX;
+	var y = event.clientY;
+	event.srcElement.style.top = (parseInt(y)-event.srcElement.getBoundingClientRect().height/2-document.getElementsByClassName("game-text")[0].getBoundingClientRect().top)+"px";
+	event.srcElement.style.left = (parseInt(x)-event.srcElement.getBoundingClientRect().width/2-document.getElementsByClassName("game-text")[0].getBoundingClientRect().left)+"px";
+
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+/*function isDrop(event)
+{
+	$("#win").dialog("open");
+}*/
